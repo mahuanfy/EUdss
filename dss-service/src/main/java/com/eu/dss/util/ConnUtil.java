f80645a5ea66a3f0692cf9ef693d94829ead5f32
821453366 <821453366@qq.com>
2017-05-25 14:15:24 +0800

    修复在ubuntu系统出现数据库插入乱码

--- dss-service/src/main/java/com/eu/dss/util/ConnUtil.java
+++ dss-service/src/main/java/com/eu/dss/util/ConnUtil.java
@@ -14,7 +14,7 @@ public class ConnUtil {
         try {
             Class.forName("com.mysql.jdbc.Driver");//1.加载数据库驱动
 
-            String url="jdbc:mysql://localhost:3306/eudsstest";//2.指定连接数据库的地址名称
+            String url="jdbc:mysql://localhost:3306/eudsstest?useUnicode=true&characterEncoding=utf-8";//2.指定连接数据库的地址名称
             String user="root";
             String passWord = "root";//指定用户名和密码
 
